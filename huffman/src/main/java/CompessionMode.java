665131621dbc4cbe5fcad720c6b31540d1059caf
Klaudia <klaudia.jozwik57@gmail.com>
2023-05-28 17:04:28 +0200

    podział na klasy

--- huffman/src/main/java/CompessionMode.java
+++ huffman/src/main/java/CompessionMode.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class CompessionMode {
+import javafx.scene.control.ComboBox;
+
+public class CompessionMode {
+    private ComboBox<String> modeComboBox;
+    private String selectedMode;
+    public CompessionMode() {
+        this.modeComboBox = new ComboBox<>(); // Inicjalizacja zmiennej modeComboBox
+    }
+    public void selectCompressionMode(){
+        modeComboBox = new ComboBox<>();
+        String[] modes = {"huffman", "huffman v2"};
+        modeComboBox.getItems().addAll(modes);
+        modeComboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            selectedMode = newValue;
+        });
+        modeComboBox.getSelectionModel().selectFirst();
+    }
+
+    public ComboBox<String> getModeComboBox() {
+        return modeComboBox;
+    }
+    public String getSelectedMode() {
+        return selectedMode;
+    }
 }
--- huffman/src/main/java/SavedFrequency.java
+++ huffman/src/main/java/SavedFrequency.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class SavedFrequency {
-}
+import java.io.*;
+import java.util.HashMap;
+
+public class SavedFrequency {
+        private String freqAll;
+
+        public void generateStats(String inputFilePath, String Mode) throws IOException, InterruptedException {
+
+            if (inputFilePath.endsWith(".txt")) {
+                String outputFilePath = "temp.bin";
+                HuffmanCompressor.runC(inputFilePath, outputFilePath, Mode);
+
+
+                HashMap< Integer, Integer > frequencyMap = DataSave.getFrequencyMap(inputFilePath);
+                StringBuilder freqAllBuilder = new StringBuilder();
+                for (int key: frequencyMap.keySet()) {
+                    String freq = (char) key + ":" + frequencyMap.get(key);
+                    freqAllBuilder.append(freq).append("\n ");
+                }
+                if (freqAllBuilder.length() > 0) {
+                    freqAllBuilder.setLength(freqAllBuilder.length() - 2);
+                }
+                freqAll = freqAllBuilder.toString();
+            } else if (inputFilePath.endsWith(".bin")) {
+                String outputFilePath = "temp.txt";
+                HuffmanDecompressor.decompress(inputFilePath, outputFilePath);
+                HashMap < Integer, Integer > frequencyMap = DataSave.getFrequencyMap(outputFilePath);
+                StringBuilder freqAllBuilder = new StringBuilder();
+                for (int key: frequencyMap.keySet()) {
+                    String freq = (char) key + ":" + frequencyMap.get(key);
+                    freqAllBuilder.append(freq).append("\n ");
+                }
+                if (freqAllBuilder.length() > 0) {
+                    freqAllBuilder.setLength(freqAllBuilder.length() - 2);
+                }
+                freqAll = freqAllBuilder.toString();
+            }
+        }
+        public String getFreqAll() {
+            return freqAll;
+        }
+    }
